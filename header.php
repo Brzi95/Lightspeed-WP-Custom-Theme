226661232e503e43d2a1c41de230b4158129f199
Milan Ivic <105347809+Brzi95@users.noreply.github.com>
2023-06-21 11:21:25 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,11 +1,9 @@
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html <?php language_attributes() ?>>
 <head>
     <meta charset="<?php bloginfo('charset') ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
-<!-- 	<meta name="google-play-app" content="app-id=com.google.android.youtube"> -->
     <title> <?php the_title(); ?> </title>
     <?php wp_head(); ?>
 </head>
